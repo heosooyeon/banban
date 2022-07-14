bbdeda66fe7bba65441e0f334c223247c82740c2
azure-553 <simmijin816@gmail.com>
2022-07-14 19:27:22 +0900

    a

---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/header"
+import { Main } from "../components/main"
+
+export const MainPage = () => {
+    return (
+        <div>
+            <Header />
+          <Main />
+        </div>
+    )
+}
