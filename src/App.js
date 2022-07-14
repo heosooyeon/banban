957a2ddba480c4f555e63db0ca1f8f58bb722d9d
heosooyeon <80025144+heosooyeon@users.noreply.github.com>
2022-07-14 11:28:52 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="footer">
+    <div id="menu">
+      <button id="mything">
+        나의 냉장고
+      </button>
+      <button id="in">
+        재료담기
+      </button>
+      <button id="my">
+        프로필
+      </button>
+
+      <button class="sign">
+        login
+      </button>
+      <button class="sign">
+        sign up
+      </button>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
